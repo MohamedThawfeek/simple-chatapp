04e2d1ee5ab4567f77bd7ae47208e44e6212af72
Mohamed Thawfeek <89966428+MohamedThawfeek@users.noreply.github.com>
2024-09-29 15:38:21 +0530

    Update axios.js

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.db_url,
+  baseURL: "https://simple-chatapp-server.vercel.app",
 });
 
 export default instance;
